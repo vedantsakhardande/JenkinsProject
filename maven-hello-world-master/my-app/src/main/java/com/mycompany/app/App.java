a7386631d538471eaeefe36677eca3f87ed76a5f
vedantsakhardande <38341087+vedantsakhardande@users.noreply.github.com>
2018-04-13 11:29:41 +0530

    Add files via upload

---
+++ maven-hello-world-master/my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int choice,n1,n2;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the number 1 :");
+	n1=sc.nextInt();
+	System.out.println("Enter the number 2 :");
+	n2=sc.nextInt();
+	System.out.println("The Addition of the two numbers is :"+(n1+n2));
+        System.out.println("A NullPointerException is a drag...");
+        Object myObject = null;
+        System.out.println("Check Our Project at https://github.com/vedantsakhardande/JenkinsProject");
+    }
+}
